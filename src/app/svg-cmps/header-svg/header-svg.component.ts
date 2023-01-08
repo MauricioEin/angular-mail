9e7dec460e4b22200640f660d800b221f8b50d97
Arnon Arditi <arnonrdt1234@gmail.com>
2023-01-08 18:23:07 +0200

    complete header ui

--- src/app/svg-cmps/header-svg/header-svg.component.ts
+++ src/app/svg-cmps/header-svg/header-svg.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,Input } from '@angular/core';
 
 @Component({
   selector: 'header-svg',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./header-svg.component.scss']
 })
 export class HeaderSvgComponent {
-
+@Input() icon!:string
 }
