257c0de217a2b2c75b6fd517bba3789bc7b1fb67
Arnon Arditi <arnonrdt1234@gmail.com>
2023-01-11 15:08:28 +0200

    a

--- src/app/pages/email-list/email-list.component.ts
+++ src/app/pages/email-list/email-list.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Store } from '@ngrx/store';
-import { Observable, pluck, lastValueFrom, Subscription } from 'rxjs';
+import { Observable, pluck } from 'rxjs';
 import { Email, selectedEmail } from 'src/app/models/email';
 import { State } from '../../store/store';
-import { LoadEmails, RemoveEmail, RemoveEmails, SetFilter } from 'src/app/store/actions/email.actions';
+import { RemoveEmail,RemoveEmails } from 'src/app/store/actions/email.actions';
 import { FilterBy } from 'src/app/models/filterBy';
 
 
@@ -19,25 +19,16 @@ export class EmailListComponent {
 
   selectedEmail!: selectedEmail | null
   selectedEmails: Array<Email> = []
-  tab!: string
-  subscription!: Subscription
-
+  tab!:string
 
   constructor(private store: Store<State>,
     private route: ActivatedRoute) {
-    this.emails$ = this.store.select('emailState').pipe(pluck('emails'));
-    this.filterBy$ = this.store.select('emailState').pipe(pluck('filterBy'));
-  }
+      this.emails$ = this.store.select('emailState').pipe(pluck('emails'));
+      this.filterBy$ = this.store.select('emailState').pipe(pluck('filterBy'));
+    }
 
-  ngOnInit() {
-    this.subscription = this.route.params.subscribe(async params => {
-      if (this.tab===params['tab']) return
-      this.tab = params['tab']
-      this.store.dispatch(new SetFilter({ txt: '', page: 0, tab: this.tab }));
-    })
-    // this.filterBy$.subscribe(filterBy=>this.store.dispatch(new LoadEmails(filterBy)))
-
-    // this.tab = this.route.snapshot.data['tab']
+  ngOnInit(){
+    this.tab=this.route.snapshot.data['tab']
   }
 
   toggleCheckbox(payload: selectedEmail): void {
@@ -45,15 +36,15 @@ export class EmailListComponent {
       this.selectedEmails.push(payload.email)
     }
     else {
-      const idx = this.selectedEmails.findIndex(e => e._id === payload.email._id)
-      this.selectedEmails.splice(idx, 1)
+      const idx=this.selectedEmails.findIndex(e=>e._id===payload.email._id)
+      this.selectedEmails.splice(idx,1)
     }
   }
 
   onRemoveEmails() {
     console.log('emailList: dispatching remove');
     this.store.dispatch(new RemoveEmails(this.selectedEmails))
-    this.selectedEmails = []
+    this.selectedEmails=[]
   }
 
 }
